e7b49fc1fb2720df0fc0c9c458db8968c72e28f5
soj1998 <736495034@qq.com>
2024-03-12 15:04:25 +0800

    3

--- gpdata.py
+++ gpdata.py
@@ -6,8 +6,15 @@ try:
     stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol=str('2529').zfill(6), period="daily",
                                             start_date="20230101", end_date='20231231',
                                             adjust="")
-    stock_zh_a_hist_df.to_excel('ceshi.xlsx')
+    xuhao = 0
+    dailydf = pd.DataFrame()
+    for row in stock_zh_a_hist_df.itertuples():
+        dailydfx = {'Index': row.Index, 'riqi': row.日期, 'shoupan': row.收盘, 'zhenfu': round(float(row.振幅)),
+                    'zhangdiefu': round(float(row.涨跌幅)),
+                    'huanshoulv': round(float(row.换手率))}
+        dailydfx1 = pd.DataFrame(dailydfx, index=[0])
+        dailydf = pd.concat([dailydf, dailydfx1])
     stock_zh_index_daily_df = ak.stock_zh_index_daily(symbol="sh000001")
     print(1)
-except:
-    print('error')
+except ValueError:
+    print('error', ValueError)
---
+++ test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {'name': ['Tom', 'Jerry'], 'age': [20, 21]}
+
+df = pd.DataFrame(data)
+
+new_data = {'name': 'Mike', 'age': 22}
+
+df._append(new_data, ignore_index=True)
+
+print(df)
